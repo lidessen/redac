6fd5feb9d27f4361242c160668534667d583e395
Aoiste <ztytotoro@outlook.com>
2022-08-13 00:28:40 +0800

    update

--- react.ts
+++ react.ts
@@ -1,16 +1,25 @@
 import { redac, watch } from "./core.ts";
 import { TYPE } from "./symbol.ts";
-import { useSyncExternalStore } from "https://esm.sh/use-sync-external-store@1.2.0/shim";
+import { useSyncExternalStoreWithSelector } from "https://esm.sh/use-sync-external-store@1.2.0/shim";
+import { equal } from "./equal.ts";
 
-export function useRedac<T>(data: T): T {
+export function useRedac<T, P = T>(data: T, selector?: (state: T) => P): P {
   const r = redac(data);
   const subscribe = (fn: () => void) => {
     return watch(r, fn);
   };
   const snapshot = () => {
-    r[TYPE] === "ref" || r[TYPE] === "getter"
-      ? Reflect.get(r, "current", r)
-      : r;
+    return (
+      r[TYPE] === "ref" || r[TYPE] === "getter"
+        ? Reflect.get(r, "current", r)
+        : r
+    ) as P;
   };
-  return useSyncExternalStore(subscribe, snapshot);
+  return useSyncExternalStoreWithSelector(
+    subscribe,
+    snapshot,
+    snapshot,
+    selector || (() => snapshot()),
+    equal
+  );
 }
